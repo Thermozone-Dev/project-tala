8a6a6891a0a6dc851321ff35a08292fc7bb2c266
joshraphaelorolfo <joshorolfo04@gmail.com>
2026-02-18 22:47:49 +0800

    trustee resource

---
+++ app/Filament/Resources/Trustees/TrusteeResource.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Resources\Trustees;
+
+use App\Filament\Resources\Trustees\Pages\CreateTrustee;
+use App\Filament\Resources\Trustees\Pages\EditTrustee;
+use App\Filament\Resources\Trustees\Pages\ListTrustees;
+use App\Filament\Resources\Trustees\Schemas\TrusteeForm;
+use App\Filament\Resources\Trustees\Schemas\TrusteeInfolist;
+use App\Filament\Resources\Trustees\Tables\TrusteesTable;
+use App\Models\Trustee;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class TrusteeResource extends Resource
+{
+    protected static ?string $model = Trustee::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUser;
+
+    public static function form(Schema $schema): Schema
+    {
+        return TrusteeForm::configure($schema);
+    }
+
+    public static function infolist(Schema $schema): Schema
+    {
+        return TrusteeInfolist::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return TrusteesTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListTrustees::route('/'),
+            'create' => CreateTrustee::route('/create'),
+            'edit' => EditTrustee::route('/{record}/edit'),
+        ];
+    }
+}
