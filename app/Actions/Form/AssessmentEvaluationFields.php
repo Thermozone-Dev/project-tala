0396e2078d0a5c53577276f4f1a7f91d46579b3b
joshraphaelorolfo <joshorolfo04@gmail.com>
2026-03-02 18:31:42 +0800

    Update AssessmentEvaluationFields.php

--- app/Actions/Form/AssessmentEvaluationFields.php
+++ app/Actions/Form/AssessmentEvaluationFields.php
@@ -5,6 +5,8 @@ namespace App\Actions\Form;
 use App\Models\EvaluationFormSection;
 use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Section;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -14,32 +16,69 @@ class AssessmentEvaluationFields
 
     public function handle($ef_id)
     {
-        $eval_form_section = EvaluationFormSection::where('evaluation_form_id',$ef_id)
+        $eval_form_sections = EvaluationFormSection::where('evaluation_form_id',$ef_id)
             ->where('section_type_id',1) // 1 = 'Assessment'
-            ->first();
+            ->get();
 
-        $rating_scale_values = $eval_form_section->ratingScale->values
-            ->mapWithKeys(function ($item) {
-                return [
-                    $item->value => $item->value . ' - ' . $item->qualitative
-                ];
-            })
-            ->toArray();
+        if ($eval_form_sections->isEmpty()) return [];
 
-        foreach ($eval_form_section->questionnaires as $question) {
-            $fields[] = Select::make($question->id)
-                ->inlineLabel()
-                ->label($question->name)
-                ->options($rating_scale_values);
+        foreach ($eval_form_sections as $index => $eval_form_section) {
+            if($index == 0){
+                $rating_scale_values = $eval_form_section->ratingScale->values
+                    ->mapWithKeys(function ($item) {
+                        return [
+                            $item->value => $item->value . ' - ' . $item->qualitative,
+                        ];
+                    })
+                    ->toArray();
+
+                foreach ($rating_scale_values as $rating_scale_value) {
+                    $rating_scales[] = Placeholder::make('placeholder')->label($rating_scale_value);
+                }
+
+                $sections[] = Section::make('Rating Scale')->columns(count($rating_scale_values))->schema($rating_scales);
+            }
+
+            foreach ($eval_form_section->questionnaires as $index2 => $question) {
+
+                if($eval_form_section->add_remarks){
+                    $fields[] = Grid::make(4)->schema([
+                        Placeholder::make('placeholder')->label($question->name)
+                            ->columnSpan(2),
+                        Select::make($question->id)
+                            ->required()
+                            ->hiddenLabel()
+                            ->validationMessages([
+                                'required' => 'This field is required.',
+                            ])
+                            ->options($rating_scale_values),
+                        Textarea::make('remarks_'.$index2)
+                            ->placeholder('Remarks...')
+                            ->hiddenLabel()
+                    ]);
+
+                }else{
+                    $fields[] = Grid::make(3)->schema([
+                        Placeholder::make('placeholder')->label($question->name)
+                            ->columnSpan(2),
+                        Select::make($question->id)
+                            ->required()
+                            ->hiddenLabel()
+                            ->validationMessages([
+                                'required' => 'This field is required.',
+                            ])
+                            ->options($rating_scale_values),
+                    ]);
+                }
+
+            }
+
+            $sections[] = Section::make($eval_form_section->title)->schema($fields)->collapsible();
+
+            $fields = [];
+
         }
 
-        foreach ($rating_scale_values as $index => $rating_scale_value) {
-            $rating_scales[] = Placeholder::make('rating_scale_'.$index)->label($rating_scale_value);
-        }
-
-        return [
-            Section::make('Rating Scale')->columns(count($rating_scale_values))->schema($rating_scales),
-            Section::make($eval_form_section->title)->schema($fields)
-        ];
+        return $sections;
     }
 }
