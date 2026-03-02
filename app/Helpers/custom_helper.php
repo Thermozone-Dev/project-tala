a7479c9086305c77eed483bd43748ed92534ee4a
joshraphaelorolfo <joshorolfo04@gmail.com>
2026-03-02 18:30:26 +0800

    Update custom_helper.php

--- app/Helpers/custom_helper.php
+++ app/Helpers/custom_helper.php
@@ -2,6 +2,7 @@
 
 use App\Models\CommitteeHasTrustee;
 use App\Models\EvaluationForm;
+use App\Models\EvaluationFormSection;
 use Spatie\Permission\Models\Role;
 
 if (! function_exists('get_eval_form_by_role')) {
@@ -57,5 +58,13 @@ if (! function_exists('check_committee_permission')) {
         $role = Role::where('id', $role_id)->first();
         return $role->hasPermissionTo($permission);
 
+    }
+}
+if (! function_exists('check_eval_form_sections')) {
+    function check_eval_form_sections($ef_id,$section_type_id)
+    {
+        return EvaluationFormSection::where('evaluation_form_id',$ef_id)
+        ->where('section_type_id',$section_type_id)
+        ->first();
     }
 }
