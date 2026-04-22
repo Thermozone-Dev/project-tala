7ec5f4ae6bc7b1780b10d62bb583511564f14ae1
joshraphaelorolfo <joshorolfo04@gmail.com>
2026-04-22 22:56:12 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/admin/evaluation-result', [EvaluationFormPDF::class, 'getEvaluationResult'])->name('queues-call-next');
 
     // Reports
-    Route::get('bot-performance-summary', [ReportsController::class, 'bot_performance_summary'])->name('bot-performance-summary');
-    Route::get('download-bot-performance-summary', [ReportsController::class, 'download_bot_performance_summary'])->name('download-bot-performance-summary');
+    Route::get('preview-report', [ReportsController::class, 'preview_report'])->name('preview-report');
 });
 
 Route::redirect('/login', '/admin/login', 301)->name('login');
