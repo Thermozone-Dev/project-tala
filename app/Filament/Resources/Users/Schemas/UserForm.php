eb8df4e8ca25718a4ebfe9f107477d44202a6e75
joshraphaelorolfo <joshorolfo04@gmail.com>
2026-05-23 16:39:38 +0800

    user fields

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -3,12 +3,16 @@
 namespace App\Filament\Resources\Users\Schemas;
 
 use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class UserForm
@@ -17,31 +21,45 @@ class UserForm
     {
         return $schema
             ->components([
-                Toggle::make('is_active')->default(true),
-                Grid::make(4)
-                    ->columnSpanFull()
-                    ->schema([
-                        TextInput::make('first_name')
+            Section::make()
+                ->columnSpanFull()
+                ->columns()
+                ->schema([
+                    Toggle::make('is_active')->default(true),
+                    Grid::make(4)
+                        ->columnSpanFull()
+                        ->schema([
+                            TextInput::make('first_name')
+                                ->required(),
+                            TextInput::make('middle_name'),
+                            TextInput::make('last_name')
+                                ->required(),
+                            TextInput::make('suffix'),
+                        ]),
+                    Group::make([
+                        TextInput::make('email')
+                            ->label('Email address')
+                            ->email()
                             ->required(),
-                        TextInput::make('middle_name'),
-                        TextInput::make('last_name')
-                            ->required(),
-                        TextInput::make('suffix'),
+                        Select::make('roles')
+                            ->relationship('roles', 'name',   fn ($query) => auth()->user()?->hasRole('Super Admin') ? $query : $query->whereNotIn('name', ['Trustee', 'Super Admin'])->orderBy('id', 'asc'))
+                            ->multiple()
+                            ->preload()
+                            ->required()
+                            ->searchable(),
                     ]),
-                TextInput::make('email')
-                    ->label('Email address')
-                    ->email()
-                    ->required(),
-                TextInput::make('password')
-                    ->revealable()
-                    ->password()
-                    ->required(),
-                Select::make('roles')
-                    ->relationship('roles', 'name',   fn ($query) => auth()->user()?->hasRole('Super Admin') ? $query : $query->whereNotIn('name', ['Trustee', 'Super Admin'])->orderBy('id', 'asc'))
-                    ->multiple()
-                    ->preload()
-                    ->required()
-                    ->searchable(),
+                    Group::make([
+                        TextInput::make('password')
+                            ->revealable()
+                            ->password()
+                            ->required(),
+                        TextInput::make('confirm_password')
+                            ->revealable()
+                            ->same('password')
+                            ->password()
+                            ->required(),
+                    ])
+                ]),
             ]);
     }
 }
