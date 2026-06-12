6281525d09c3e40e19a5272b5f60ad9cb68ee30b
joshraphaelorolfo <joshorolfo04@gmail.com>
2026-06-12 23:08:40 +0800

    models

---
+++ app/Models/Meeting.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+class Meeting extends Model
+{
+    use LogsActivity;
+
+    protected $fillable = [
+        'title',
+        'meeting_type_id',
+        'committee_id',
+        'meeting_link',
+        'scheduled_at',
+        'created_by',
+    ];
+
+    protected $casts = [
+        'scheduled_at' => 'datetime',
+    ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['title', 'meeting_type_id', 'committee_id', 'meeting_link', 'scheduled_at'])
+            ->logOnlyDirty();
+    }
+
+    public function meetingType(): BelongsTo
+    {
+        return $this->belongsTo(MeetingType::class);
+    }
+
+    public function committee(): BelongsTo
+    {
+        return $this->belongsTo(Committee::class);
+    }
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function attendees(): HasMany
+    {
+        return $this->hasMany(MeetingAttendee::class);
+    }
+}
